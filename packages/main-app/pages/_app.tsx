8bc625ee6be12299c1dfc1a298d8de4d8aec43c4
Kevin Chow <kschow48@gmail.com>
2021-06-10 14:11:05 -0500

    add data-builder package

--- packages/main-app/pages/_app.tsx
+++ packages/main-app/pages/_app.tsx
@@ -1,12 +1,9 @@
 import React, { FunctionComponent } from 'react';
+import { AppProps } from 'next/app';
 import '../styles.scss';
 
-type MyAppProps = {
-    Component: React.ComponentType;
-    pageProps: object;
-}
 // This default export is required in a new `pages/_app.js` file.
-const MyApp: FunctionComponent<MyAppProps> = ({ Component, pageProps }) => {
+const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
     return <Component {...pageProps} />;
 };
 
